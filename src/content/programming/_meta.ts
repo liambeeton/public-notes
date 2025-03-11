c9529b94d196284ff551a91df1090550a4166a79
Liam Beeton <liambeeton@users.noreply.github.com>
2025-03-11 13:44:09 +0200

    Add public note about building a local rag app (#1)

--- src/content/programming/_meta.ts
+++ src/content/programming/_meta.ts
@@ -1,5 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 
 export default {
+  'building-a-lightweight-rag-system-with-llamaindex-ollama-and-qdrant':
+    'Building a Lightweight RAG System with LlamaIndex, Ollama, and Qdrant',
   'secure-grpc-over-mtls-using-go': 'Secure gRPC over mTLS using Go',
 }
